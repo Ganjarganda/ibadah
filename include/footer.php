85a88fe0ce0f3a0e3e3ae8f828659a1775bfc3d3
Ganjarganda <ganjar15ti@mahasiswa.pcr.ac.id>
2018-12-27 06:35:15 +0700

    v1
    baru bisa checklist

---
+++ include/footer.php
@@ -0,0 +1,20 @@
+<!-- Call to Action Section -->
+<div class="row mb-4">
+  <div class="col-md-8">
+    <p>Website Ini Merupakan Sistem Informasi Geografis Tempat Ibadah Berbagai Agama.</p>
+  </div>
+  <div class="col-md-4">
+    <a class="btn btn-lg btn-secondary btn-block" href="index.php">Refresh</a>
+  </div>
+</div>
+
+</div>
+<!-- /.container -->
+
+<!-- Footer -->
+<footer class="py-5 bg-dark">
+  <div class="container">
+    <p class="m-0 text-center text-white">Copyright &copy; GIS:IBADAH 2018</p>
+  </div>
+  <!-- /.container -->
+</footer>
---
+++ index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<html>
+<head>
+  <meta charset="utf-8" />
+  <title>GIS:IBADAH</title>
+  <meta name="viewport" content="initial-scale=1.0, user-scalable=no, width=device-width">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ol3/4.0.1/ol.css" />
+  <link rel="stylesheet" href="css/ol3-layerswitcher.css" />
+  <link rel="stylesheet" href="css/layerswitcher.css" />
+
+  <!-- Bootstrap core CSS -->
+  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+  <!-- Custom styles for this template -->
+  <link href="css/modern-business.css" rel="stylesheet">
+
+</head>
+<body>
+
+  <!-- Navigation -->
+  <?php include('include/navigation.php'); ?>
+
+  <!-- Page Content -->
+  <div class="container-fluid">
+
+    <h1 class="my-1">Tempat Ibadah</h1>
+
+    <!-- Map -->
+    <div class="row">
+
+      <div id="map"></div>
+
+    </div>
+    <!-- /.row -->
+
+    <hr>
+
+    <?php include('include/footer.php'); ?>
+
+
+    <!-- The line below is only needed for old environments like Internet Explorer and Android 4.x -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdn.polyfill.io/v2/polyfill.min.js?features=requestAnimationFrame,Element.prototype.classList,URL"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/ol3/4.0.1/ol.js"></script>
+
+    <!-- Layer Switcher / Filter / Checklist -->
+    <?php include('js/ol3-layerswitcher.php'); ?>
+    <?php include('js/layerswitcher.php'); ?>
+
+
+  </body>
+  </html>
